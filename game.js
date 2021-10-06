344dc201e6cf27237b04c5f332cd002e2dbca3fa
Juan Manuel Otegui <juanma.otegui@gmail.com>
2021-10-06 16:14:01 -0300

    Fix play and highscores buttons

--- game.js
+++ game.js
@@ -150,7 +150,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('./end.html')
     }
 
     questionCounter++
