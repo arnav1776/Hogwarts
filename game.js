36adc67fc15720d4972f59cd0eb9bd78bc232612
Ayush guha <91629926+HelloGit-ty@users.noreply.github.com>
2021-10-06 22:04:53 +0530

    Updated game.js

--- game.js
+++ game.js
@@ -43,11 +43,56 @@ let questions = [
         choice3: "Salazar",
         choice4: "Riddle",
         answer: 1,
-    }
+    },
+    {
+        question:
+            "What were the middle names Albus Dumbledore had? (Hint- He told it in front of the Minister of Magic in Order of Phoneix)",
+        choice1: "Percival Wulfric Brian",
+        choice2: "Percival Brian",
+        choice3: "Wulfric Brian",
+        choice4: "None of the above",
+        answer: 1,
+    },
+    {
+        question:
+            "Which of these are the ingredients for the Polyjuice Potion?",
+        choice1: "Lacewing flies(stewed for 10 days), Boomslang skin",
+        choice2: "Gillyweed, knotgrass",
+        choice3: "Lacewing flies(stewed for 21 days), shredded Boomslang skin",
+        choice4: "McNuggets and fries",
+        answer: 3,
+    },
+    {
+        question:
+            "A spell when cast, causes severe haemmorhage, and lacerates the target. What spell am i talking bout?",
+        choice1: "Crucio",
+        choice2: "Rictumsempra",
+        choice3: "Sectumsempra",
+        choice4: "Alahamora",
+        answer: 3,
+    },
+    {
+        question:
+            "What are the names of Severus Snape's parents?",
+        choice1: "Tobias Snape and Eileen Snape",
+        choice2: "Tripp snape and Eliana Snape",
+        choice3: "Not mentioned in the book",
+        choice4: "Theodore Snape and Eveleen Snape",
+        answer: 1,
+    },
+    {
+        question:
+            "How many exceptions are there to Gamp's Elemental Law?",
+        choice1: "3",
+        choice2: "6",
+        choice3: "2",
+        choice4: "5",
+        answer: 4,
+    },
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 225
+const MAX_QUESTIONS = 9
 
 startGame = () => {
     questionCounter = 0
